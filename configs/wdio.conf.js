5516dbe42af53d99fdc9380a6bea2b1736e88869
Vladyslav Bilous <vladeezym@gmail.com>
2023-07-12 15:02:52 +0300

    123

--- configs/wdio.conf.js
+++ configs/wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/*.js'
+        '../test/specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -94,7 +94,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: process.env.SITE === 'prod' ?'https://prod.telnyx.com/' : 'https://stag.telnyx.com/',
+    baseUrl: process.env.SITE === 'https://telnyx.com/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
