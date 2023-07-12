242820b5ebfbc740a848d3f5e09e88aadb2c27d5
Vladyslav Bilous <vladeezym@gmail.com>
2023-07-12 14:55:09 +0300

    123

--- configs/wdio.conf.js
+++ configs/wdio.conf.js
@@ -4,7 +4,7 @@ exports.config = {
     // Runner Configuration
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
-    runner: 'local',
+   // runner: 'local',
     
     //
     // ==================
@@ -53,7 +53,8 @@ exports.config = {
     //
     capabilities: [{
         // capabilities for local browser web tests
-        browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+        browserName: 'chrome', // or "firefox", "microsoftedge", "safari",
+        acceptInsecureCerts: true
     }],
     //
     // ===================
@@ -102,7 +103,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver','docker'],
+    services: ['chromedriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
